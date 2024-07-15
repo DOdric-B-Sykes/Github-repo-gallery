9737fd2aaaaeaa0acfe157eecc32899d0559e8a0
D_Sykes <Itachi_15_90@yahoo.com>
2024-07-15 18:42:28 -0500

    Populated repo info

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 /* Variable to select the div element with a class = overview; This is where the profile infromation will appear when popluated */
 const overview = document.querySelector(".overview");
-/* Variable for my Github username */
+/* Global variable for my Github username */
 const userName = "DOdric-B-Sykes";
+/* Global variable to select the unordered list to display the repos list */
+const displayRepo = document.querySelector(".repo-list");
+
 
 /* 1st function needed to access and pull information from Githiub */
 const gitProfile = async function (){
@@ -19,7 +22,7 @@ const displayData = function (gitData){
     let newDiv = document.createElement("div"); /* Created new div element */
     newDiv.classList.add("user-info");
     newDiv.innerHTML = `<figure> 
-    <img alt="user avatar" src=${"https://avatars.githubusercontent.com/u/74344387?v=4"} />
+    <img alt="user avatar" src= ${"https://avatars.githubusercontent.com/u/74344387?v=4"} />
   </figure>
   <div>
     <p><strong>Name:</strong> ${"D'Odric B. Sykes"}</p>
@@ -30,4 +33,25 @@ const displayData = function (gitData){
   </div>`;
 
   overview.append(newDiv); /* Adds newly created div and its contents from the "innerhTML" to the overview variable that targets the HTML element with the "overview" class */
+
+  fetchRepo();
+};
+
+/* Function to fetch repo data */
+const fetchRepo = async function (){
+    const repoList = await fetch (`https://api.github.com/users/${userName}/repos?sort=updated&per_page=100`); /* API call with endpoints (users/username/repos) and parameters that start after the "?" (sort=updated&per_page=100 - "updated" represent the value for addressing the sort parameter and "100" to represent the value for addressing the per_page paramter) to display the repos in a manner that is sorted from most recently updated to last & from listed from 0 to 100 representing the number of repos to fetch/display -> in the form of an array */
+    const repoData = await repoList.json();
+    console.log(repoData);
+
+    displayRepoInfo(repoData);
+};
+
+/* Function to display fetched repo information for each repo */
+const displayRepoInfo = function (repoData){
+    repoData.forEach(function (repo){ /* Used forEach loop to loop through each element in the "repoData" varaible/array, and exectuted the code beneath it for each element in the array, using "repo" has a parameter to represent the individual elements in the array */
+        repoListItem = document.createElement("li");
+        repoListItem.classList.add("repo");
+        repoListItem.innerHTML = `<h3>${repo.name}</h3>`; /* Use "repo" in repo.name because I want to access the name property of "repo" (the object in this instance) -> via the "repo" parameter in the forEach loop (in which the value for "repo" is defined as each repo/element object in the repoData array) and the "repoData" parameter (repoData variable/array) in the "displayRepoInfo" function */
+        displayRepo.append(repoListItem); /* Add newly created li (list item/repoListItem) to the unordered list where thr repos are to be displayed via the displayRepo variable */
+    })
 };
