95681ded7c1b9529b22ee103e21a4e59d517f2e3
D_Sykes <Itachi_15_90@yahoo.com>
2024-07-17 16:22:02 -0500

    Updated JS

--- js/script.js
+++ js/script.js
@@ -4,11 +4,15 @@ const overview = document.querySelector(".overview");
 const userName = "DOdric-B-Sykes";
 /* Global variable to select the unordered list to display the repos list */
 const displayRepo = document.querySelector(".repo-list");
+/* Global variable that selects the section where all my repo info will appears */
+const repoAppear = document.querySelector(".repos");
+/* Global variable that selects the section where the individual repo info wiil appear */
+const individualRepoData = document.querySelector(".repo-data");
 
 
 /* 1st function needed to access and pull information from Githiub */
 const gitProfile = async function (){
-    const userProfile = await fetch(`https://api.github.com/users/${userName}`); /* Used templat literal here in order to have the ability to access the userName variable in my fetch command */
+    const userProfile = await fetch(`https://api.github.com/users/${userName}`); /* API call - Used templat literal here in order to have the ability to access the userName variable in my fetch command - which will allow me to fetch and access the github profile associated with the value of the "userName" variable */
     const gitData = await userProfile.json();
     console.log(gitData); /* Logged gitData out so my console can then see the properties that where fetched from the userProfile variable */
 
@@ -22,14 +26,13 @@ const displayData = function (gitData){
     let newDiv = document.createElement("div"); /* Created new div element */
     newDiv.classList.add("user-info");
     newDiv.innerHTML = `<figure> 
-    <img alt="user avatar" src= ${"https://avatars.githubusercontent.com/u/74344387?v=4"} />
+    <img alt="user avatar" src= ${gitData.avatar_url} />
   </figure>
   <div>
-    <p><strong>Name:</strong> ${"D'Odric B. Sykes"}</p>
-    <p><strong>Bio:</strong> ${
-        "Hello, I am an aspiring coder looking to learn, grow and further develop my skills in web development, design and beyond.\r\nSkilled in: HTML, CSS, Git and JS"}</p>
-    <p><strong>Location:</strong> ${"Houston, TX"}</p>
-    <p><strong>Number of public repos:</strong> ${34}</p>
+    <p><strong>Name:</strong> ${gitData.name}</p>
+    <p><strong>Bio:</strong> ${gitData.bio}</p>
+    <p><strong>Location:</strong> ${gitData.location}</p>
+    <p><strong>Number of public repos:</strong> ${gitData.public_repos}</p>
   </div>`;
 
   overview.append(newDiv); /* Adds newly created div and its contents from the "innerhTML" to the overview variable that targets the HTML element with the "overview" class */
@@ -55,3 +58,48 @@ const displayRepoInfo = function (repoData){
         displayRepo.append(repoListItem); /* Add newly created li (list item/repoListItem) to the unordered list where thr repos are to be displayed via the displayRepo variable */
     })
 };
+
+/* Event listener for click event on the unordered list for the displayRepo variable */
+displayRepo.addEventListener("click", function (e){
+    if (e.target.matches("h3")){ /* .matches() checks if the target matches a specific element (the h3 element in this case) */
+        let repoName = e.target.innerText; /* Sets the value of the "repoName" variable to the innerText where the event happens, in this case that would be the h3 element */
+        specRepoName(repoName);
+    }
+});
+
+/* Function to fetch specific repo information */
+const specRepoName = async function (repoName){
+    const specRepoInfo = await fetch(`https://api.github.com/repos/${userName}/${repoName}`); /* API call - Written this way to fetch and access information about whatever repo I click on (via a click event and displayRepo event listener) */
+    const repoInfo = await specRepoInfo.json();
+    console.log(repoInfo);
+
+    /* Used to grab coding languages present in construction of repo */
+    const fetchLang = await fetch(repoInfo.languages_url); /* Fetch the language_url property of whatever repo is clicked on via the "repoInfo" variable and "specRepoName" variable */
+    const langData = await fetchLang.json();
+    console.log(langData);
+
+    /* Adding languages to an array */
+    const langArray = []; /* Empty array that will display the coding languages used in a specific repo when clicked via a click event */
+    for (let language in langData){ /* Used a for...in loop to loop through the "landData" variable since its value is now an object via the "langData" and "fetchLang" variables -> "language" ,in this stance, is a placeholder that represents the key/property associated with the "langData" variable/object via the "fetchLang", "repoInfo" and "specRepoInfo" variables -> The "for...in" loop is telling the console that whatever key/property, multiple keys/properties in this instance, (represented by the "language" variable) is present in the "langData" object, loop through each key/property element of the "landData" variable/array */
+        langArray.push(language); /* Whatever the value that is associated with the "language" key/property, is pushed/added to the "langArray" variable/array -> Meaning that for every key/property that is looped through via the "for...in" loop, add/push those keys to the "langArray" variable/array */
+    }
+
+    console.log(langArray);
+
+    displaySpecRepoInfo(repoInfo, langArray);
+};
+
+/* Function to display specific repo info */
+const displaySpecRepoInfo = function(repoInfo, langArray){
+    individualRepoData.innerHTML = "";
+    let newIndividualRepoDiv = document.createElement("div");
+    newIndividualRepoDiv.innerHTML = `<h3>Name: ${repoInfo.name}</h3>
+    <p>Description: ${repoInfo.description}</p>
+    <p>Default Branch: ${repoInfo.default_branch}</p>
+    <p>Languages: ${langArray.join(", ")}</p> 
+    <a class="visit" href="${repoInfo.url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>`; /* langArray.join(", ") tells the console to join the values of the "langArray" variable/array into a string */
+    
+    individualRepoData.append(newIndividualRepoDiv);
+    individualRepoData.classList.remove("hide");
+    repoAppear.classList.add("hide");
+}; 
